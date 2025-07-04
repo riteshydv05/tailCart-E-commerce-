c2d98871600b8661ce1af1fce55e53628e10074e
riteshydv05 <riteshyvns2005@gmail.com>
2025-07-04 11:32:19 +0530

    Update Products.jsx

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -3,14 +3,14 @@ import Heading from './Heading'
 import ProductCard from './ProductCard';
 
 const ProductsData = [
-  { id: 1, name: 'Camera', price: '$99',image:"../../public/Cam.png" },
-  { id: 2, name: 'RTX 4090', price: '$99',image:"../../public/Rtx.png" },
-  { id: 3, name: 'Mechanical Keyboard', price: '$99', image:"../../public/Keybg.png" },
-  { id: 4, name: 'Monitor', price: '$99', image:"../../public/Monitor.png" },
-  { id: 5, name: 'iPhone 16 Pro Max', price: '$99', image:"../../public/Iphone.png" },
-  { id: 6, name: 'Vision Pro', price: '$99', image:"../../public/Vision.png" },
-  { id: 7, name: 'Wireless Earbuds', price: '$99', image:"../../public/Buds.png" },
-  { id: 8, name: 'Smartwatch', price: '$99', image:"../../public/Swatchbg.png" },
+  { id: 1, name: 'Camera', price: '$99',image:"/Cam.png" },
+  { id: 2, name: 'RTX 4090', price: '$99',image:"/Rtx.png" },
+  { id: 3, name: 'Mechanical Keyboard', price: '$99', image:"/Keybg.png" },
+  { id: 4, name: 'Monitor', price: '$99', image:"/Monitor.png" },
+  { id: 5, name: 'iPhone 16 Pro Max', price: '$99', image:"/Iphone.png" },
+  { id: 6, name: 'Vision Pro', price: '$99', image:"/Vision.png" },
+  { id: 7, name: 'Wireless Earbuds', price: '$99', image:"/Buds.png" },
+  { id: 8, name: 'Smartwatch', price: '$99', image:"/Swatchbg.png" },
 ];
 function Products() {
   return (
